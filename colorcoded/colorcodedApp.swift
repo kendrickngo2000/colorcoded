103cfbbea46c25f6ce540940efd278b9cffa0bdc
Kendrick Ngo <kendrickngo2000@aim.com>
2024-02-01 00:54:15 -0800

    Initial Commit

---
+++ colorcoded/colorcodedApp.swift
@@ -0,0 +1,17 @@
+//
+//  colorcodedApp.swift
+//  colorcoded
+//
+//  Created by Kendrick Ngo on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct colorcodedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
