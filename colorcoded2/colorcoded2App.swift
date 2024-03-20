28e0661a3b761f4eb1a5dbf83f97bc1e9ebeb3fb
Kendrick Ngo <kendrickngo2000@aim.com>
2024-03-19 21:18:48 -0700

    Initial Commit

---
+++ colorcoded2/colorcoded2App.swift
@@ -0,0 +1,17 @@
+//
+//  colorcoded2App.swift
+//  colorcoded2
+//
+//  Created by Kendrick Ngo on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct colorcoded2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ colorcoded2UITests/colorcoded2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  colorcoded2UITestsLaunchTests.swift
+//  colorcoded2UITests
+//
+//  Created by Kendrick Ngo on 3/19/24.
+//
+
+import XCTest
+
+final class colorcoded2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
